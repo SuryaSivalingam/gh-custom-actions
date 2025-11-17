b7d1e06a2063528db65f4dfc9453a2ebc0c9029d
surya <suryas1697@gmail.com>
2025-11-17 22:58:54 +0530

    JS custom actions yml completed [no ci]

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,8 +1,24 @@
 const core=require('@actions/core');
-const github=require('@actions/github');
+// const github=require('@actions/github');
 const exec=require('@actions/exec');
+
 function run(){
-   core.notice('Hello from JavaScript Custom Action');
+   
+    core.notice('Hello from JavaScript Custom Action');
+
+    //Getting inputs
+    const bucketName = core.getInput('bucket-name', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { requiredd: false });
+    const artifactFolder = core.getInput('artifact-folder' , { required: true });
+    
+    // Upload artifacts
+    const s3Uri = `s3://${bucketName}`;
+    exec.exec(`aws s3 sync ${artifactFolder} ${s3Uri} --region ${bucketRegion}`);
+    
+    // Website URL
+    const websiteUrl = `http://${bucketName}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url',websiteUrl);
+
 }
 
 run();
